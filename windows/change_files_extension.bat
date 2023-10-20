6368f8e7e7dcb042606871868addce01942cf95e
Sergio Calahorra <sergiocalahorra20@gmail.com>
2023-10-20 21:49:42 +0200

    Change echo message

--- windows/change_files_extension.bat
+++ windows/change_files_extension.bat
@@ -6,11 +6,11 @@ set /p original_extension=Introduce la extensión origen de los archivos:
 set /p final_extension=Introduce la extensión destino de los archivos: 
 
 for /r %%F in (*.%original_extension%) do (
-    set "filepath=%%~dpF"
-    set "filename=%%~nF"
-    set "extension=%%~xF"
-    echo Renombrando: "%%~nxF" a "!filename!.%final_extension%"
-    ren "%%F" "!filename!.%final_extension%"
+  set "filepath=%%~dpF"
+  set "filename=%%~nF"
+  set "extension=%%~xF"
+  echo Cambiando extensión del archivo "!filename!" de "!original_extension!" a "!final_extension!"
+  ren "%%F" "!filename!.%final_extension%"
 )
 
 endlocal
